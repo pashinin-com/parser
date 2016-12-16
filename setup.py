e4045b7d2b7938cf1688104882efd5df8aa80c33
Sergey Pashinin <sergey@pashinin.com>
2016-12-17 00:01:47 +0300

    fixed comma

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
