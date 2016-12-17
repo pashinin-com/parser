a26a13f6cd4fc219409d72d8e3e960d17f246ac2
Sergey Pashinin <sergey@pashinin.com>
2016-12-18 02:46:53 +0300

    sudo install-rust

--- install-rust.sh
+++ install-rust.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-set -e -x
-curl https://static.rust-lang.org/rustup.sh > /tmp/rustup.sh
-chmod +x /tmp/rustup.sh
-/tmp/rustup.sh -y --disable-sudo --channel=$1
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+# set -e -x
+# curl https://static.rust-lang.org/rustup.sh > /tmp/rustup.sh
+# chmod +x /tmp/rustup.sh
+# /tmp/rustup.sh -y --disable-sudo --channel=$1
