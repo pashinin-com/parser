78312625f8df1acc02890ea7943c5d2cabce36c4
Sergey Pashinin <sergey@pashinin.com>
2016-12-18 02:51:13 +0300

    Revert "sudo install-rust"
    This reverts commit a26a13f6cd4fc219409d72d8e3e960d17f246ac2.

--- install-rust.sh
+++ install-rust.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-# set -e -x
-# curl https://static.rust-lang.org/rustup.sh > /tmp/rustup.sh
-# chmod +x /tmp/rustup.sh
-# /tmp/rustup.sh -y --disable-sudo --channel=$1
+set -e -x
+curl https://static.rust-lang.org/rustup.sh > /tmp/rustup.sh
+chmod +x /tmp/rustup.sh
+/tmp/rustup.sh -y --disable-sudo --channel=$1
