ba5dd12ea075e0aaafa45dcfe0c6488bf73a6f65
Sergey Pashinin <sergey@pashinin.com>
2017-01-18 16:39:25 +0300

    latex.rs

---
+++ src/latex.rs
@@ -0,0 +1,21 @@
+//! LaTeX parser
+
+use std::collections::HashSet;
+use std::collections::HashMap;
+use std::convert::From;
+use std::borrow::Cow;
+
+
+pub struct Latex<'a> {
+    src: Cow<'a, str>,
+}
+
+
+
+impl<'a> From<Cow<'a, str>> for Latex<'a> {
+    fn from(src: Cow<'a, str>) -> Latex<'a> {
+        Latex {
+            src: src
+        }
+    }
+}
