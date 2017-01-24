259243bf3432e5ecab52d56d9d2dfd292d2842a5
Sergey Pashinin <sergey@pashinin.com>
2017-01-24 21:08:34 +0300

    new year

--- rparser/__init__.py
+++ rparser/__init__.py
@@ -5,7 +5,7 @@ __version__ = '0.1.16'
 # __build__ = 0x021203
 __author__ = 'Sergey Pashinin'
 __license__ = 'GPL 3.0'
-__copyright__ = 'Copyright 2016 Sergey Pashinin'
+__copyright__ = 'Copyright 2017 Sergey Pashinin'
 
 
 try:
